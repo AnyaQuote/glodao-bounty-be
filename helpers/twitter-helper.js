bcffddf369cfb6f21902b89a2decec0cc6cc928a
Dao Quoc Toan <daoqtoan@gmail.com>
2022-03-20 00:36:18 +0700

    feat: add utilities function for twitter helper

--- helpers/twitter-helper.js
+++ helpers/twitter-helper.js
@@ -3,10 +3,17 @@ const axios = require("axios");
 const TWITTER_API_BEARER_TOKEN =
   "AAAAAAAAAAAAAAAAAAAAALuUaQEAAAAAI7IuqJFxrj4iWWQHxJA5rTIKWRM%3D7vpzpWFdqpjuhKqdCm0Jr1BSVhnfc8mIKjPTkvWUmLjOqHBwEw";
 
+const axiosInstance = axios.create({
+  timeout: 1000,
+  headers: {
+    Authorization: `Bearer ${TWITTER_API_BEARER_TOKEN}`,
+  },
+});
+
 const isTwitterStatusLink = (link) => {
   if (
     link.match(
-      /^((?:http:\/\/)?|(?:https:\/\/)?)?(?:www\.)?twitter\.com\/(?:#!\/)?(\w+)\/status(es)?\/(\d+)/i
+      /^((?:http:\/\/)?|(?:https:\/\/)?)?((?:www\.)?|(?:mobile\.)?)?twitter\.com\/(?:#!\/)?(\w+)\/status(es)?\/(\d+)/i
     ) ||
     link.match(/^@?(\w+)$/)
   )
@@ -15,7 +22,6 @@ const isTwitterStatusLink = (link) => {
 };
 
 const getTweetDataByTweetId = async (id) => {
-  console.log(id);
   try {
     const { data } = await axios.get(`https://api.twitter.com/2/tweets/${id}`, {
       headers: {
@@ -28,7 +34,32 @@ const getTweetDataByTweetId = async (id) => {
   }
 };
 
+const getTweetData = async (
+  id,
+  tweetFields = "attachments,author_id,created_at,public_metrics,source,referenced_tweets",
+  expansions = "",
+  params = {}
+) => {
+  try {
+    const { data } = await axiosInstance.get(
+      `https://api.twitter.com/2/tweets`,
+      {
+        params: {
+          ids: id,
+          "tweet.fields": tweetFields,
+          expansions,
+          ...params,
+        },
+      }
+    );
+    return data.data[0];
+  } catch (error) {
+    throw error;
+  }
+};
+
 const getTweetIdFromLink = (link) => {
+  if (!isTwitterStatusLink(link)) return "";
   const splitedArr = link.split("/");
   return splitedArr[splitedArr.length - 1].split("?")[0];
 };
@@ -37,4 +68,5 @@ module.exports = {
   isTwitterStatusLink,
   getTweetDataByTweetId,
   getTweetIdFromLink,
+  getTweetData,
 };
