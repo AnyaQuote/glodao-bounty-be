cb3bf9b22d093a7f5ab63f663669b90bdf19ab80
Dao Quoc Toan <daoqtoan@gmail.com>
2022-03-22 16:30:40 +0700

    feat: create update wallet route

--- api/hunter/controllers/hunter.js
+++ api/hunter/controllers/hunter.js
@@ -26,6 +26,27 @@ module.exports = {
       id: id,
     });
 
+    return await verifySoliditySignature(
+      walletAddress,
+      signature,
+      hunter.nonce
+    );
+  },
+  updateWalletAddress: async (ctx) => {
+    const { walletAddress, signature, id, chain } = ctx.request.body;
+
+    if (!walletAddress || !signature || !id || !chain)
+      return ctx.badRequest("Invalid request body: missing fields");
+
+    if (!isEqual(chain, "sol") && !isSolidityAddress(walletAddress))
+      return ctx.badRequest(
+        "Invalid wallet address: wallet address is not ETH chain"
+      );
+
+    const hunter = await strapi.services.hunter.findOne({
+      id: id,
+    });
+
     const isValidSignature = await verifySoliditySignature(
       walletAddress,
       signature,
