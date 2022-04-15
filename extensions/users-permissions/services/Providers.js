d4fdbb78930b51da30a7c6fe2a3f272186ffb2a7
Dao Quoc Toan <daoqtoan@gmail.com>
2022-04-15 16:23:13 +0700

    quick fix: login email error

--- extensions/users-permissions/services/Providers.js
+++ extensions/users-permissions/services/Providers.js
@@ -54,7 +54,7 @@ const connect = (provider, query) => {
           referrerCode = "######";
 
         const users = await strapi.query("user", "users-permissions").find({
-          email: profile.email,
+          twitterId: profile.twitterId,
         });
 
         const advanced = await strapi
