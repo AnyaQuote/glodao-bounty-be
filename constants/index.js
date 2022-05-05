eabe2a55cd89e28d3ad8bd29703a2110b9559fef
Dao Quoc Toan <daoqtoan@gmail.com>
2022-05-05 10:44:15 +0700

    feat: add constants

--- constants/index.js
+++ constants/index.js
@@ -1,5 +1,10 @@
 const FIXED_NUMBER = require("./fixedNumber");
 
+const GLODAO_ROOT_CODE = "5mWd4O";
+
+const GLODAO_BASE_ADDRESS = "0x7a05CE29a44cA8dD49D967367F98D3F07E204faC";
+
 module.exports = {
   FIXED_NUMBER,
+  GLODAO_ROOT_CODE,
 };
