0d1c763d4489b6bb31675b03f5200e084120bde1
Dao Quoc Toan <daoqtoan@gmail.com>
2022-05-19 15:22:36 +0700

    feat: create quiz verify route

--- api/quiz/services/quiz.js
+++ api/quiz/services/quiz.js
@@ -1,8 +1,20 @@
-'use strict';
-
+"use strict";
+const { isArrayIncluded } = require("../../../helpers");
 /**
  * Read the documentation (https://strapi.io/documentation/developer-docs/latest/development/backend-customization.html#core-services)
  * to customize this service
  */
 
-module.exports = {};
+/**
+ * Check if submited answers is correct or not
+ * @param {array} base the base array contains all answer
+ * @param {array} compare submited answers
+ * @returns true if all the submited answer is correct, false otherwise
+ */
+const verifyQuizAnswer = (base, compare) => {
+  return isArrayIncluded(compare, base);
+};
+
+module.exports = {
+  verifyQuizAnswer,
+};
