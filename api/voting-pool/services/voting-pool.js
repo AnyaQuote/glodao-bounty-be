45eaf06d7dae2e574c48e0fc04c0d3085b9cf140
Anh Tu <n.anhtu0101@gmail.com>
2022-05-25 22:27:34 +0700

    update voting pool info

--- api/voting-pool/services/voting-pool.js
+++ api/voting-pool/services/voting-pool.js
@@ -3,7 +3,6 @@ const {
   getPoolInfo,
 } = require("../../../helpers/blockchainHelpers/voting-helper");
 
-const { isEqual, set } = require("lodash");
 const checkIsOwner = (ctx, ownerAddress) => {
   if (ctx.state.user.username !== ownerAddress)
     return ctx.forbidden(`You can not update this entry`);
