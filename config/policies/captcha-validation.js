563a720b5c1e12db884a8797f3272789d7baba61
Dao Quoc Toan <daoqtoan@gmail.com>
2022-06-03 17:27:52 +0700

    feat: create captcha validation policy

---
+++ config/policies/captcha-validation.js
@@ -0,0 +1,17 @@
+"use strict";
+
+const _ = require("lodash");
+const { verifyCaptchaToken } = require("../../helpers/hcaptcha-helper");
+
+module.exports = async (ctx, next) => {
+  const captchaToken =
+    _.get(ctx, "query.captchaToken", "") ||
+    _.get(ctx, "request.body.captchaToken", "") ||
+    _.get(ctx, "params.captchaToken", "");
+
+  const isCaptchaValid = await verifyCaptchaToken(captchaToken);
+  if (!isCaptchaValid.success)
+    return ctx.badRequest("Wrong captcha validation");
+
+  return await next();
+};
