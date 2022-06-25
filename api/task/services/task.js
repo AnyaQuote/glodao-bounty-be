b57ea6c12e4fd223e7e350acd8a15fcc14472138
duchm22 <duchm.se22@gmail.com>
2022-06-25 19:18:50 +0700

    chore: remove null bounty from applies, refactor forEach with reduce, split method

--- api/task/services/task.js
+++ api/task/services/task.js
@@ -3,7 +3,7 @@
 const { get, gte, isEmpty } = require("lodash");
 const moment = require("moment");
 const { FixedNumber } = require("@ethersproject/bignumber");
-
+const fxZero = FixedNumber.from("0");
 /**
  * Read the documentation (https://strapi.io/documentation/developer-docs/latest/development/backend-customization.html#core-services)
  * to customize this service
@@ -101,6 +101,9 @@ const isTaskProcessable = (task) => {
   return moment().isBetween(moment(task.startTime), moment(task.endTime));
 };
 
+const POOL_NOT_FOUND = "Could not find pool to create this mission";
+const INVALID_DATE_RANGE = "Invalid mission start and end date";
+const EXCEEDED_MISSION_LIMIT = "You have reached missions limit";
 
 /**
  * Create task services
@@ -130,20 +133,20 @@ const createTask = async (ctx, missionData) => {
   });
 
   if (isEmpty(pool)) {
-    ctx.badRequest("Could not find pool to create this mission");
+    ctx.badRequest(POOL_NOT_FOUND);
   }
 
   if (
     moment(startTime).isBefore(moment(pool.startDate)) ||
     moment(endTime).isAfter(moment(pool.endDate))
   ) {
-    ctx.badRequest("Invalid mission start and end date");
+    ctx.badRequest(INVALID_DATE_RANGE);
   }
 
   const numberOfCreatedMissions = await strapi.services.task.count({ poolId });
 
   if (numberOfCreatedMissions >= pool.totalMission) {
-    ctx.forbidden("You have reached missions limit");
+    ctx.forbidden(EXCEEDED_MISSION_LIMIT);
   }
 
   return await strapi.services.task.create({
@@ -162,47 +165,77 @@ const createTask = async (ctx, missionData) => {
     data,
     metadata,
   });
-}
+};
 
-  const calculateAverageCommunityReward = async (
-    _limit,
-    type = "bounty",
-    _sort = "endTime:DESC",
-    isEnded = true
-  ) => {
-    const tasks = await strapi.services.task.find({
-      _limit,
-      type: type ? type : undefined,
-      _sort,
-      endTime_lte: isEnded ? moment().toISOString() : undefined,
-    });
-    const applies = await Promise.all(
-      tasks.map((task) => {
-        return strapi.services.apply.findOne({
-          task: task.id,
-          status: "awarded",
-          poolType: "community",
-        });
-      })
-    );
-    let result = FixedNumber.from("0");
-    applies.forEach((apply) => {
-      const optionalTokenReward = get(apply, "optionalTokenReward", []);
-      let optionalTokenTotalValue = FixedNumber.from("0");
-      optionalTokenReward.forEach((token) => {
-        optionalTokenTotalValue = optionalTokenTotalValue.addUnsafe(
-          FixedNumber.from(`${token.bounty}`).mulUnsafe(
-            FixedNumber.from(`${token.tokenBasePrice}`)
-          )
-        );
+/**
+ * Convert token to base price and sum converted value
+ * @param {array} optionalTokenReward array of token
+ * @returns sum of converted value
+ */
+const calculateOptionTokenTotalValue = (optionalTokenReward) => {
+  const totalValue = optionalTokenReward.reduce((acc, token) => {
+    const fxTokenBounty = FixedNumber.from(`${token.bounty}`);
+    const fxTokenBasePrice = FixedNumber.from(`${token.tokenBasePrice}`);
+    const fxConvertedValue = fxTokenBasePrice.mulUnsafe(fxTokenBounty);
+    return acc.addUnsafe(fxConvertedValue);
+  }, fxZero);
+  return totalValue;
+};
+
+/**
+ * Calculate average community reward from tasks and applies
+ * @param {number} limit number of task/mission to get
+ * @param {string} type type of task/mission, default is 'bounty'
+ * @param {string} sort sort order, default will sort by tasks's end time in descending order
+ * @param {boolean} isEnded should only get task that has ended or not, default is true
+ * @returns average community reward
+ */
+const calculateAverageCommunityReward = async (
+  limit,
+  type = "bounty",
+  sort = "endTime:DESC",
+  isEnded = true
+) => {
+  // Get lastest bounty missions/tasks based on limit amount
+  const tasks = await strapi.services.task.find({
+    type,
+    _limit: limit,
+    _sort: sort,
+    endTime_lte: isEnded ? moment().toISOString() : undefined,
+  });
+
+  // Get all applies with status of 'awarded' and poolType 'community' from tasks
+  const applies = await Promise.all(
+    tasks.map((task) => {
+      return strapi.services.apply.findOne({
+        task: task.id,
+        status: "awarded",
+        poolType: "community",
       });
-      result = result
-        .addUnsafe(optionalTokenTotalValue)
-        .addUnsafe(FixedNumber.from(`${apply.bounty}`));
-    });
-    return result.divUnsafe(FixedNumber.from(`${_limit}`))._value;
-  }
+    })
+  );
 
+  // Filter null result in applies
+  const nonNullApplies = applies.filter((x) => x);
+
+  const fxNumOfApplies = FixedNumber.from(`${nonNullApplies.length}`);
+
+  // Get sum of reward value from applies
+  const sum = nonNullApplies.reduce((accumlator, apply) => {
+    const optionalTokenTotalValue = calculateOptionTokenTotalValue(
+      get(apply, "optionalTokenReward", [])
+    );
+    const fxApplyBounty = FixedNumber.from(`${apply.bounty}`);
+    return accumlator
+      .addUnsafe(optionalTokenTotalValue)
+      .addUnsafe(fxApplyBounty);
+  }, fxZero);
+
+  // Div to get average reward value
+  const averageValue = sum.divUnsafe(fxNumOfApplies)._value;
+
+  return averageValue;
+};
 
 module.exports = {
   increaseTaskTotalParticipants,
