04ec14c704b387abfe2b045600759addb4336def
duchm22 <duchm.se22@gmail.com>
2022-06-28 09:43:04 +0700

    chore: remove create hunter in user lifecycle hook

--- extensions/users-permissions/models/User.js
+++ extensions/users-permissions/models/User.js
@@ -1,7 +1,5 @@
 "use strict";
 const _ = require("lodash");
-const { generateRandomNonce } = require("../../../helpers/wallet-helper");
-const EMPTY_CODE = "######";
 
 /**
  * Lifecycle callbacks for the `User` model.
@@ -9,63 +7,6 @@ const EMPTY_CODE = "######";
 
 module.exports = {
   lifecycles: {
-    // Called after an entry is created
-    async afterCreate(result) {
-      const { referrerCode, id, username, referralCode, avatar } = result;
-      try {
-        const campaign = await strapi.services.campaign.findOne({
-          code: referrerCode,
-        });
-        if (campaign) {
-          await strapi.services.hunter.create({
-            name: username,
-            status: "active",
-            user: id,
-            nonce: generateRandomNonce(),
-            referralCode,
-            referrerCode: campaign.owner.referralCode,
-            campaignCode: campaign.code,
-            hunterRole: "user",
-            root: campaign.owner.referralCode,
-            metadata: {
-              avatar,
-            },
-          });
-        } else {
-          let root = EMPTY_CODE;
-          let campaignCode = "######";
-          if (referrerCode !== EMPTY_CODE) {
-            const referrer = await strapi.services.hunter.findOne({
-              referralCode: referrerCode,
-            });
-            if (!_.isEmpty(referrer)) {
-              root = referrer.root;
-              campaignCode = referrer.campaignCode;
-            }
-          }
-          await strapi.services.hunter.create({
-            name: username,
-            status: "active",
-            user: id,
-            nonce: generateRandomNonce(),
-            referralCode,
-            referrerCode,
-            campaignCode: campaignCode,
-            hunterRole: "user",
-            root: root,
-            metadata: {
-              avatar,
-            },
-          });
-        }
-      } catch (error) {
-        await strapi.query("user", "users-permissions").delete({ id });
-        throw new Error(
-          "[INFO]Cannot create user right now. Please try again!"
-        );
-      }
-    },
-
     async afterDelete(result) {
       await strapi.services.hunter.delete({ id: result.hunter.id });
     },
