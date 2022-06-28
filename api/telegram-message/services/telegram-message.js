74a36b7717a15c891d64496f5ae443625e1f1b0d
Bang Bu <daoqtoan@gmail.com>
2022-06-28 15:18:08 +0700

    feat: create service verify telegram chat task

--- api/telegram-message/services/telegram-message.js
+++ api/telegram-message/services/telegram-message.js
@@ -1,8 +1,52 @@
-'use strict';
+"use strict";
 
 /**
  * Read the documentation (https://strapi.io/documentation/developer-docs/latest/development/backend-customization.html#core-services)
  * to customize this service
  */
 
-module.exports = {};
+// is existed ["telegram-message"] by chatUsername and userId _limit 1
+const isExistedTelegramMessage = async (chatUsername, userId) => {
+  try {
+    const res = await strapi.services["telegram-message"].findOne({
+      chatUsername,
+      userId,
+    });
+    return res;
+  } catch (error) {
+    throw error;
+  }
+};
+
+// delete telegram message by id
+const deleteTelegramMessage = async (id) => {
+  try {
+    return await strapi.services["telegram-message"].delete({ id });
+  } catch (error) {
+    throw error;
+  }
+};
+
+//verify telegram chat task
+const verifyTelegramChatTask = async (chatUsername, userId) => {
+  try {
+    const res = await isExistedTelegramMessage(chatUsername, userId);
+    if (res) {
+      await deleteTelegramMessage(res.id);
+      return res;
+    } else {
+      return null;
+    }
+  } catch (error) {
+    throw error;
+  }
+};
+
+// get chatUsername from telegram link
+const getChatUsernameFromLink = (link) => {
+  if (!isTelegramChatLink(link)) return "";
+  const splitedArr = link.split("/");
+  return splitedArr[splitedArr.length - 1].split("?")[0];
+};
+
+module.exports = { verifyTelegramChatTask };
