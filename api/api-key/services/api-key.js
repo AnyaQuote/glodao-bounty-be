ca9dc05a6034cc0b9e8bfa640965c9f18a3da2f7
Bang Bu <daoqtoan@gmail.com>
2022-07-03 15:29:36 +0700

    feat: create generate api key service

--- api/api-key/services/api-key.js
+++ api/api-key/services/api-key.js
@@ -1,8 +1,51 @@
-'use strict';
+"use strict";
+
+const { generateRandomString } = require("../../../helpers");
+const {
+  BASE_API_KEY_LENGTH,
+  BASE_API_SECRET_KEY_LENGTH,
+} = require("../../../constants");
 
 /**
  * Read the documentation (https://strapi.io/documentation/developer-docs/latest/development/backend-customization.html#core-services)
  * to customize this service
  */
 
-module.exports = {};
+/**
+ * generate api key and secret key
+ * @param {string} projectOwner project owner's id
+ * @param {array} taskIds array of task ids
+ * @param {array} routes array of routes
+ * @returns new api key
+ */
+const generateApiKeyWithRoutes = async (
+  projectOwner,
+  taskIds,
+  routes = [
+    {
+      path: "/tasks/updateInAppTrial",
+      method: "POST",
+      description: "UpdateInAppTrialTask",
+      isPublic: true,
+      isProtected: false,
+    },
+  ]
+) => {
+  const key =
+    generateRandomString(BASE_API_KEY_LENGTH) +
+    "-" +
+    generateRandomString(BASE_API_KEY_LENGTH);
+  const secret = generateRandomString(BASE_API_SECRET_KEY_LENGTH);
+  const apiKey = await strapi.services["api-key"].create({
+    routes: routes,
+    projectOwner,
+    key,
+    secret,
+    tasks: taskIds.map((id) => ({ id: id, code: generateRandomString() })),
+  });
+  return apiKey;
+};
+
+module.exports = {
+  generateApiKeyWithRoutes,
+};
