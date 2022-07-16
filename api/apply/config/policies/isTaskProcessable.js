15dceece7430bf09760907bde485126c84dfc8f7
Bang Bu <daoqtoan@gmail.com>
2022-07-16 10:23:58 +0700

    feat: update task processable policy

--- api/apply/config/policies/isTaskProcessable.js
+++ api/apply/config/policies/isTaskProcessable.js
@@ -20,13 +20,23 @@ module.exports = async (ctx, next) => {
     const taskRecord = await strapi.services.task.findOne({ id: task });
     const taskParticipantLimit = _.get(taskRecord, "maxParticipants", 0);
     if (taskParticipantLimit < 1) return await next();
-    else if (taskRecord.type === "learn") {
+    else {
       const completedCount = await strapi.services.apply.count({
         status_ne: "processing",
         task: task,
       });
       if (completedCount >= taskParticipantLimit)
         return ctx.badRequest("The mission has fulled");
+    }
+    const uniqueSubTypeId = _.get(taskRecord, "metadata.uniqueSubTypeId", "");
+    if (!_.isEmpty(uniqueSubTypeId)) {
+      const hunterId = _.get(ctx, "state.user.hunter", "");
+      const hunterRecord = await strapi.services.hunter.findOne({
+        id: hunterId,
+      });
+      const uniqueTaskIds = _.get(hunterRecord, "data.uniqueTaskIds", []);
+      if (uniqueTaskIds.includes(uniqueSubTypeId))
+        return ctx.badRequest("You have already applied for this mission");
     }
   } else if (!_.isEmpty(id)) {
     const apply = await strapi.services.apply.findOne({ id: id });
@@ -37,7 +47,7 @@ module.exports = async (ctx, next) => {
     });
     const taskParticipantLimit = _.get(taskRecord, "maxParticipants", 0);
     if (taskParticipantLimit < 1) return await next();
-    else if (taskRecord.type === "learn") {
+    else {
       const completedCount = await strapi.services.apply.count({
         status_ne: "processing",
         task: taskId,
