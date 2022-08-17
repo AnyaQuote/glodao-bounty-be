e037d1971b467045c03267c70e2cb220c36fe2b4
Bang Bu <daoqtoan@gmail.com>
2022-08-17 11:47:56 +0700

    feat: add route to send email

---
+++ api/email/controllers/Email.js
@@ -0,0 +1,30 @@
+"use strict";
+const _ = require("lodash");
+
+/**
+ * Read the documentation () to implement custom controller functions
+ */
+
+module.exports = {
+  /**
+   * Sends an email to the recipient in the body of the request
+   */
+  contact: async (ctx) => {
+    try {
+      const { fname, email, description } = ctx.request.body;
+      await strapi.plugins["email"].services.email.send({
+        to: "glodao.dev@gmail.com",
+        subject: "User Contact",
+        text: `User Contact send from https://glodao.io\nFullname: ${fname}\nEmail: ${email}\nMessage: ${description}`,
+        from: "noreply@glodao.io",
+      });
+      return {
+        status: 200,
+        message: "Email sent successfully",
+      };
+    } catch (error) {
+      strapi.log.debug(error);
+      ctx.send({ error: "Error sending mail" });
+    }
+  },
+};
