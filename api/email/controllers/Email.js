5181cbbd72b1ca7d1b037e0c3464504e2ca66ee8
Bang Bu <daoqtoan@gmail.com>
2022-08-25 15:52:17 +0700

    feat: update send email route

--- api/email/controllers/Email.js
+++ api/email/controllers/Email.js
@@ -1,5 +1,6 @@
 "use strict";
 const _ = require("lodash");
+const moment = require("moment");
 
 /**
  * Read the documentation () to implement custom controller functions
@@ -11,15 +12,15 @@ module.exports = {
    */
   contact: async (ctx) => {
     try {
-      console.log(ctx.request.body);
-      console.log(typeof ctx.request.body);
-      const { fname, email, description } = ctx.request.body;
-      console.log(fname, email, description);
+      let requestBody = ctx.request.body;
+      if (typeof ctx.request.body === "string")
+        requestBody = JSON.parse(ctx.request.body);
+      const { fname, email, description } = requestBody;
       await strapi.plugins["email"].services.email.send({
-        to: "glodao.dev@gmail.com",
-        subject: "User Contact",
-        text: `User Contact send from https://glodao.io\nFullname: ${fname}\nEmail: ${email}\nMessage: ${description}`,
-        from: "noreply@glodao.io",
+        to: "hello@glodao.io",
+        bcc: "glodao.dev@gmail.com",
+        subject: `User Contact`,
+        text: `User Contact send from https://glodao.io\nFullname: ${fname}\nEmail: ${email}\nMessage: ${description}\nTime: ${moment()}`,
       });
       console.log("send email finished");
       return {
