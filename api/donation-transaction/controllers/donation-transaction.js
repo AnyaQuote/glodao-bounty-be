2f131c1a5b0f1e34239a95396f202bd5934db042
Bang Bu <daoqtoan@gmail.com>
2022-08-30 19:44:10 +0700

    feat: create route to record donation

--- api/donation-transaction/controllers/donation-transaction.js
+++ api/donation-transaction/controllers/donation-transaction.js
@@ -1,8 +1,23 @@
-'use strict';
+"use strict";
+const { isEmpty } = require("lodash");
 
 /**
  * Read the documentation (https://strapi.io/documentation/developer-docs/latest/development/backend-customization.html#core-controllers)
  * to customize this controller
  */
 
-module.exports = {};
+/**
+ * Record donation
+ * @param {Context} ctx context
+ * @returns new record
+ */
+const recordDonation = async (ctx) => {
+  const { tx } = ctx.request.body;
+  if (isEmpty(tx)) return ctx.badRequest(null, "Transaction hash is required");
+
+  return await strapi.services["donation-transaction"].recordDonation(tx);
+};
+
+module.exports = {
+  recordDonation,
+};
