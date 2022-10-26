46b1dff70832ffb4695c2a6e45afb5972ecef6f7
Bang Bu <daoqtoan@gmail.com>
2022-10-26 11:22:31 +0700

    feat(cron): update platform on 26 11

--- config/functions/cron.js
+++ config/functions/cron.js
@@ -17,6 +17,88 @@ const { getTweetIdFromLink } = require("../../helpers/twitter-helper");
  * See more details here: https://strapi.io/documentation/developer-docs/latest/setup-deployment-guides/configurations.html#cron-tasks
  */
 
+const updateUserPlatform = async () => {
+  const users = await strapi
+    .query("user", "users-permissions")
+    .find({ _limit: -1 });
+  console.log(users.length);
+  const chunks = _.chunk(users, 99);
+  let index = 0;
+  for (const subChunk of chunks) {
+    index = index + 1;
+    await Promise.all(
+      subChunk.map((user) => {
+        return strapi.query("user", "users-permissions").update(
+          { id: user.id },
+          {
+            platform: "gld",
+          }
+        );
+      })
+    ).then(() => {
+      console.log("batch user update completed", index);
+    });
+  }
+};
+
+const updateTaskPlatform = async () => {
+  const tasks = await strapi.services.task.find({ _limit: -1 });
+  console.log(tasks.length);
+  const chunks = _.chunk(tasks, 99);
+  let index = 0;
+  for (const subChunk of chunks) {
+    index = index + 1;
+    await Promise.all(
+      subChunk.map((task) => {
+        return strapi.services.task.update(
+          { id: task.id },
+          {
+            platform: "gld",
+          }
+        );
+      })
+    ).then(() => {
+      console.log("batch task update completed", index);
+    });
+  }
+};
+
+const updateApplyPlatform = async () => {
+  let applies = [];
+  const limit = 5000;
+  let _start = 0;
+  let index = 1;
+  do {
+    console.log(index);
+    const res = await strapi.services.apply.find({
+      _limit: limit,
+      _start,
+    });
+    applies = applies.concat(res);
+    _start += limit;
+    if (res.length < limit) break;
+    index++;
+  } while (true);
+  const chunks = _.chunk(applies, 99);
+  index = 0;
+  for (const subChunk of chunks) {
+    index = index + 1;
+    await Promise.all(
+      subChunk.map((apply) => {
+        return strapi.services.apply.update(
+          { id: apply.id },
+          {
+            platform: "gld",
+            IsCreatedOnTaskPlatform: true,
+          }
+        );
+      })
+    ).then(() => {
+      console.log("batch apply update completed", index);
+    });
+  }
+};
+
 module.exports = {
   "0 0 * * *": {
     task: async () => {
@@ -203,6 +285,16 @@ module.exports = {
       } finally {
         console.log("End of update task twitter participant");
       }
+    },
+    options: {
+      tz: "Asia/Bangkok",
+    },
+  },
+  "30 11 26 * *": {
+    task: async () => {
+      await updateUserPlatform();
+      await updateTaskPlatform();
+      await updateApplyPlatform();
     },
     options: {
       tz: "Asia/Bangkok",
