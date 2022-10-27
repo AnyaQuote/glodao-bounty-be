1c6e170dcbec5668dd1d17e990e9f9feef4fad4a
Bang Bu <daoqtoan@gmail.com>
2022-10-27 10:53:46 +0700

    update for multi platform

--- config/env/development/middleware.js
+++ config/env/development/middleware.js
@@ -1,11 +1,14 @@
 module.exports = {
   load: {
-    before: ["boom", "sentry"],
+    before: ["platformOrigin", "boom", "sentry"],
   },
   settings: {
     sentry: {
       dsn: "https://71ef579530144a4db4bcfd686a3607c9@o1211930.ingest.sentry.io/6349722",
       enabled: true,
+    },
+    platformOrigin: {
+      enabled: false,
     },
   },
 };
--- config/env/production/middleware.js
+++ config/env/production/middleware.js
@@ -1,11 +1,14 @@
 module.exports = {
   load: {
-    before: ["boom", "sentry"],
+    before: ["platformOrigin", "boom", "sentry"],
   },
   settings: {
     sentry: {
       dsn: "https://a529c6eef5644fb6b07351fbd30bb400@o1211930.ingest.sentry.io/6349707",
       enabled: true,
+    },
+    platformOrigin: {
+      enabled: false,
     },
   },
 };
