39f5ea512fbdaa4209efa946e0c7786526002324
Anya Quote <anya.quote@gmail.com>
2023-07-30 19:37:46 +0700

    build:turn off sentry

--- config/env/development/middleware.js
+++ config/env/development/middleware.js
@@ -1,11 +1,11 @@
 module.exports = {
-  load: {
-    before: ["boom", "sentry"],
-  },
-  settings: {
-    sentry: {
-      dsn: "https://71ef579530144a4db4bcfd686a3607c9@o1211930.ingest.sentry.io/6349722",
-      enabled: true,
-    },
-  },
+  // load: {
+  //   before: ["boom", "sentry"],
+  // },
+  // settings: {
+  //   sentry: {
+  //     dsn: "https://71ef579530144a4db4bcfd686a3607c9@o1211930.ingest.sentry.io/6349722",
+  //     enabled: true,
+  //   },
+  // },
 };
--- middlewares/sentry/index.js
+++ middlewares/sentry/index.js
@@ -1,33 +1,33 @@
-const Sentry = require("@sentry/node");
-Sentry.init({
-  dsn: strapi.config.middleware.settings.sentry.dsn,
-  environment: strapi.config.environment,
-});
+// const Sentry = require("@sentry/node");
+// Sentry.init({
+//   dsn: strapi.config.middleware.settings.sentry.dsn,
+//   environment: strapi.config.environment,
+// });
 
-module.exports = (strapi) => {
-  return {
-    initialize() {
-      strapi.app.use(async (ctx, next) => {
-        try {
-          await next();
-        } catch (error) {
-          const { user } = ctx.state;
-          const { query, params, request, response } = ctx;
-          Sentry.configureScope(function (scope) {
-            scope.setUser({ ...user, ip_address: ctx.request.ip });
-            scope.setRequestSession(request);
-            scope.setTransactionName(`${request.route.endpoint}`);
-            scope.setContext("CLIENT SYSTEM", {
-              "user-agent": request.header["user-agent"],
-            });
-            scope.setContext("request", request);
-            scope.setContext("response", response);
-            scope.setExtras({ query, params, "request body": request.body });
-          });
-          Sentry.captureException(error);
-          throw error;
-        }
-      });
-    },
-  };
-};
+// module.exports = (strapi) => {
+//   return {
+//     initialize() {
+//       strapi.app.use(async (ctx, next) => {
+//         try {
+//           await next();
+//         } catch (error) {
+//           const { user } = ctx.state;
+//           const { query, params, request, response } = ctx;
+//           Sentry.configureScope(function (scope) {
+//             scope.setUser({ ...user, ip_address: ctx.request.ip });
+//             scope.setRequestSession(request);
+//             scope.setTransactionName(`${request.route.endpoint}`);
+//             scope.setContext("CLIENT SYSTEM", {
+//               "user-agent": request.header["user-agent"],
+//             });
+//             scope.setContext("request", request);
+//             scope.setContext("response", response);
+//             scope.setExtras({ query, params, "request body": request.body });
+//           });
+//           Sentry.captureException(error);
+//           throw error;
+//         }
+//       });
+//     },
+//   };
+// };
